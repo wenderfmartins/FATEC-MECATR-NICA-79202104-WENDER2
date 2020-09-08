ffc9a8076d47faea111dca33f39207f943e3c155
WENDER FERREIRA MARTINS <70581628+wenderfmartins@users.noreply.github.com>
2020-09-08 08:18:49 -0300

    Create Programa 01.py

---
+++ LtpI-2020 02/Pratica 03/Programa 01.py
@@ -0,0 +1,5 @@
+# Isso é um comentário em Phyton.
+# A função print escreve coisas na tela. print Ola mundo
+print("Hello Word")
+nome = input("digite seu nome")
+print("Bom dia Sr", nome , "Em que posso ajudá-lo?" )
